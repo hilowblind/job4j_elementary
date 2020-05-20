6ee5f9f75d8874ab1575b961c90bdd965340406c
hilowblind <arsenikum0>
2020-05-20 22:11:02 +0400

    3.3. Разбор первой программы. [#293562]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Älexey Shkirmanov");
+        System.out.println("27.06.1990");
+    }
+}
