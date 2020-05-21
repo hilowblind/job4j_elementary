58c0e3b2a5a0455a0a7656fffbd04b2ccb664d84
hilowblind <arsenikum0>
2020-05-21 16:51:01 +0400

    6. Идеальный вес. [#293543]

---
+++ src/ru/job4j/condition/SqArea.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+import java.util.Scanner;
+
+public class SqArea {
+    public static double square(int p, int k) {
+        int h = p / (2 * (k + 1));  //finding height
+        int l = h * k;  // finding length
+        double s = l * h;  // finding square
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("print perimeter");
+        int p = scanner.nextInt();
+        System.out.println("print k");
+        int k = scanner.nextInt();
+
+        double result1 = SqArea.square(p, k);
+        System.out.println("Square = " + result1);
+    }
+}
