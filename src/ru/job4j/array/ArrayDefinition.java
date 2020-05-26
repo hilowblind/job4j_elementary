ff4caf509f88394a99a53e555ab5f928fabfea7a
hilowblind <arsenikum0>
2020-05-26 08:31:52 +0400

    6.0.2. Заполнение массива. [#293531]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        /**
+         * Contains names and surnames
+         */
+        String[] names = new String[4];
+        names[0] = "Alexey Shkirmanov";
+        names[1] = "Ïvan Ivanov";
+        names[2] = "Petr Petrov";
+        names[3] = "Roman Romanov";
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
